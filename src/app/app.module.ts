d97f85e61237694edcf1f5adf22b1afa2a39447c
Azhan Alam (CONSUMER) <AZ20054228@wipro.com>
2019-05-27 19:02:35 +0530

    login,header and trunking-add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatSelectModule} from '@angular/material/select';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material';
+import { OwlDateTimeModule, OwlNativeDateTimeModule } from 'ng-pick-datetime';
+import {MatTableModule} from '@angular/material/table';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { TrunkingAddComponent } from './trunking-add/trunking-add.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    
+    AppComponent,
+    HeaderComponent,
+    TrunkingAddComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    MatTableModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    OwlDateTimeModule,
+    OwlNativeDateTimeModule,
+    MatSelectModule,
+    MatGridListModule,
+    MatInputModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatToolbarModule,
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/trunking-add/trunking-add.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trunking-add',
+  templateUrl: './trunking-add.component.html',
+  styleUrls: ['./trunking-add.component.css']
+})
+
+
+
+export class TrunkingAddComponent implements OnInit {
+
+ 
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
