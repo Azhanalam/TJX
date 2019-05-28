64649f8f7035b67078c1123ca66f91d261b59c06
Azhan Alam (CONSUMER) <AZ20054228@wipro.com>
2019-05-28 08:52:53 +0530

    app,login,trunkin-add and assets

--- src/app/trunking-add/trunking-add.component.ts
+++ src/app/trunking-add/trunking-add.component.ts
@@ -1,5 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface PeriodicElement {
+  radio:string;
+  trunkorigin:string;
+  trunkdestination:string;
+  trunktype: string;
+  trailer:string;
+  comment:string;
+}
+const ELEMENT_DATA: PeriodicElement[] = [
+  {radio: '1', trunkorigin: 'Hatfield-LC', trunkdestination: 'Stoke on Trent-PC', trunktype: 'Empty TLHU Trunk',trailer:'TKD0448',comment:''},
+  {radio: '1', trunkorigin: 'Stoke On Trent-PC', trunkdestination: 'Hatfield-LC', trunktype: 'Stock Store Trunk',trailer:'TK4468',comment:''}
+];
+
+
 @Component({
   selector: 'app-trunking-add',
   templateUrl: './trunking-add.component.html',
@@ -7,10 +21,14 @@ import { Component, OnInit } from '@angular/core';
 })
 
 
+  
+
+
 
 export class TrunkingAddComponent implements OnInit {
 
- 
+  displayedColumns: string[] = ['radio', 'trunkorigin', 'trunkdestination', 'trunktype','trailer','comment'];
+  dataSource = ELEMENT_DATA;
 
   constructor() { }
 
